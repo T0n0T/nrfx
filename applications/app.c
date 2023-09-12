393bbd528fdcfa9f8a99df91c153e8544f39c4bf
T0n0T <823478402@qq.com>
2023-09-12 16:07:10 +0800

    feat: :art: add sm4flah to decide the mode of sm4

--- applications/app.c
+++ applications/app.c
@@ -37,98 +37,46 @@ const static uint8_t key[] = {
     0xfe, 0x7b, 0x48, 0x73,
     0xdd, 0x59, 0x5c, 0xff,
     0xf6, 0x5f, 0x58, 0xec};
-/*
-static uint8_t cipher[] = {
-    0x76, 0xf8, 0x89, 0xca,
-    0x5b, 0x8f, 0xd3, 0x16,
-    0xc8, 0x44, 0x84, 0x2f,
-    0xe2, 0xb9, 0xe8, 0xdc,
-    0xde, 0x82, 0x52, 0x91,
-    0x4e, 0x87, 0x1b, 0x88,
-    0x4f, 0x8b, 0x2a, 0x9f,
-    0x09, 0x91, 0xd8, 0xc8,
-    0xc6, 0x98, 0x57, 0xe8,
-    0xa2, 0x26, 0x5d, 0x3d,
-    0x78, 0xbd, 0x21, 0xcc,
-    0xea, 0x5b, 0x62, 0xe3,
-    0xc7, 0x8d, 0xe1, 0x2b,
-    0xa8, 0xe7, 0x7c, 0x27,
-    0x4c, 0x35, 0x80, 0x07,
-    0x4d, 0x8d, 0x87, 0xd8,
-    0x51, 0x28, 0xad, 0x2a,
-    0x26, 0xf3, 0x0c, 0xb2,
-    0x6a, 0xb4, 0x15, 0xde,
-    0xd3, 0xa0, 0xb9, 0x3c,
-    0x6f, 0x84, 0xe1, 0x0f,
-    0x6c, 0x17, 0xdf, 0xc2,
-    0x62, 0xed, 0x67, 0x65,
-    0xff, 0xcf, 0xe3, 0x96,
-    0xaa, 0xb7, 0xea, 0xd3,
-    0x3b, 0x30, 0x9a, 0x87,
-    0x07, 0x74, 0x5e, 0x13,
-    0x37, 0x6a, 0x6d, 0x5a,
-    0x0b, 0x58, 0x1a, 0xce,
-    0x28, 0x8a, 0x97, 0x25,
-    0x02, 0x95, 0x48, 0xec,
-    0xc0, 0x6d, 0x86, 0xc7,
-    0x44, 0x0f, 0x69, 0x8e,
-    0xae, 0x10, 0xf7, 0x3b,
-    0xa2, 0x42, 0x5f, 0xeb,
-    0x73, 0x5d, 0x79, 0xce,
-    0xd9, 0x63, 0xcb, 0xfc,
-    0x83, 0xa3, 0x38, 0xb7,
-    0x87, 0xd4, 0x8a, 0xdb,
-    0xa7, 0xb7, 0x1a, 0x3c,
-    0x66, 0xfb, 0x55, 0x3c,
-    0x41, 0xc2, 0x40, 0x72,
-    0xb3, 0x87, 0x69, 0xe4,
-    0x40, 0x3b, 0x2f, 0x59,
-    0xb7, 0x09, 0xee, 0x82,
-    0x07, 0x9a, 0x20, 0xc9,
-    0xa1, 0x12, 0xe5, 0x86,
-    0xf9, 0x70, 0x7e, 0xa3,
-    0x42, 0xbb, 0x4c, 0xd7,
-    0x08, 0xba, 0x98, 0x55,
-    0xa5, 0x3a, 0xa7, 0x84,
-    0x0d, 0x5b, 0x69, 0x64,
-    0xf4, 0x5e, 0x24, 0x3c,
-    0x9d, 0xf4, 0x94, 0x76,
-    0x20, 0x4c, 0x8b, 0x7a,
-    0xf7, 0x3c, 0x55, 0x04};
-*/
+
+static uint8_t sm4_flag = 0;
 
 char *build_msg_updata(char *device_id, struct LOC_GNSS *info, int battry, int pulse_rate);
 
 static void publish_handle(void)
 {
-    mqtt_error_t err       = KAWAII_MQTT_SUCCESS_ERROR;
-    char *publish_data     = build_msg_updata(DEVICE_ID, ec800x_get_gnss(), 99, 78);
-    pdata origin_mqtt      = {rt_strlen(publish_data), (uint8_t *)publish_data};
-    ciphertext cipher_mqtt = ccm3310_sm4_encrypt(sm4_id, origin_mqtt);
+    mqtt_error_t err   = KAWAII_MQTT_SUCCESS_ERROR;
+    char *publish_data = build_msg_updata(DEVICE_ID, ec800x_get_gnss(), 99, 78);
+    if (sm4_flag) {
+        pdata origin_mqtt      = {rt_strlen(publish_data), (uint8_t *)publish_data};
+        ciphertext cipher_mqtt = ccm3310_sm4_encrypt(sm4_id, origin_mqtt);
 
-    // printf("\n========= publish print encrypt ===========\n");
-    // if (cipher_mqtt.len >= 0) {
-    //     for (size_t i = 0; i < cipher_mqtt.len; i++) {
-    //         printf("%02x", *(cipher_mqtt.data + i));
-    //     }
-    // }
-    // printf("\n===================================\n");
-
-    if (cipher_mqtt.len > 0) {
+        if (cipher_mqtt.len > 0) {
+            memset(&publish_msg, 0, sizeof(publish_msg));
+            publish_msg.qos        = QOS0;
+            publish_msg.payloadlen = cipher_mqtt.len;
+            publish_msg.payload    = cipher_mqtt.data;
+            err                    = mqtt_publish(client, MQTT_TOPIC_DATA, &publish_msg);
+            if (err != KAWAII_MQTT_SUCCESS_ERROR) {
+                LOG_E("publish msg fail, err[%d]", err);
+            } else {
+                LOG_D("publish msg success!!!!!!!!");
+            }
+        } else {
+            LOG_E("publish msg sm4 encrypt fail.");
+        }
+    } else {
         memset(&publish_msg, 0, sizeof(publish_msg));
         publish_msg.qos        = QOS0;
-        publish_msg.payloadlen = cipher_mqtt.len;
-        publish_msg.payload    = cipher_mqtt.data;
+        publish_msg.payloadlen = rt_strlen(publish_data);
+        publish_msg.payload    = publish_data;
         err                    = mqtt_publish(client, MQTT_TOPIC_DATA, &publish_msg);
         if (err != KAWAII_MQTT_SUCCESS_ERROR) {
             LOG_E("publish msg fail, err[%d]", err);
-            free(publish_data);
-            return;
+        } else {
+            LOG_D("publish msg success!!!!!!!!");
         }
-        LOG_D("publish msg success!!!!!!!!");
-    } else {
-        LOG_E("publish msg sm4 encrypt fail.");
     }
+
     free(publish_data);
 }
 
@@ -136,15 +84,22 @@ static void sub_handle(void *client, message_data_t *msg)
 {
     (void)client;
     LOG_I("-----------------------------------------------------------------------------------");
-    pdata origin_mqtt = {(int)msg->message->payloadlen, (uint8_t *)msg->message->payload};
-
-    plaintext plain_mqtt = ccm3310_sm4_decrypt(sm4_id, origin_mqtt);
-    LOG_I("%s:%d %s()...\ntopic: %s\nmessage:", __FILE__, __LINE__, __FUNCTION__, msg->topic_name);
-    if (plain_mqtt.len >= 0) {
-        for (size_t i = 0; i < plain_mqtt.len; i++) {
-            printf("%c", *(plain_mqtt.data + i));
+    if (sm4_flag) {
+        pdata origin_mqtt    = {(int)msg->message->payloadlen, (uint8_t *)msg->message->payload};
+        plaintext plain_mqtt = ccm3310_sm4_decrypt(sm4_id, origin_mqtt);
+        LOG_I("%s:%d %s()...\ntopic: %s\nmessage:", __FILE__, __LINE__, __FUNCTION__, msg->topic_name);
+        if (plain_mqtt.len >= 0) {
+            for (size_t i = 0; i < plain_mqtt.len; i++) {
+                printf("%c", *(plain_mqtt.data + i));
+            }
+        }
+    } else {
+        LOG_I("%s:%d %s()...\ntopic: %s\nmessage:", __FILE__, __LINE__, __FUNCTION__, msg->topic_name);
+        for (size_t i = 0; i < msg->message->payloadlen; i++) {
+            printf("%c", *((char *)msg->message->payload + i));
         }
     }
+
     LOG_I("-----------------------------------------------------------------------------------");
 }
 
@@ -220,6 +175,7 @@ void mission_init(void)
         mqtt_release(client);
         rt_free(client);
     }
+    sm4_flag = 1;
 }
 
 void mission_deinit(void)
@@ -257,6 +213,21 @@ char *build_msg_updata(char *device_id, struct LOC_GNSS *info, int battry, int p
     return out;
 }
 
+void sm4flag(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("sm4flag on/off      - using cmd to change mode of sm4\n");
+        return;
+    }
+    if (!rt_strncmp("on", argv[1], 3)) {
+        printf("sm4 encrypt mode ON !!!!!!!!!\n");
+        sm4_flag = 1;
+    } else if (!rt_strncmp("off", argv[1], 3)) {
+        printf("sm4 encrypt mode OFF!!!!!!!!!\n");
+        sm4_flag = 0;
+    }
+}
+MSH_CMD_EXPORT(sm4flag, test);
 MSH_CMD_EXPORT_ALIAS(publish_handle, publish, test);
 MSH_CMD_EXPORT(mission_init, test);
 MSH_CMD_EXPORT(mission_deinit, test);
